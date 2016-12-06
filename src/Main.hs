2afd2cc4684d1fd2a3715a0c88d957a5dd31720b
William Van Steen <williamv@slalom.com>
2016-12-05 16:23:56 -0800

    Added Vigenere cipher.  Updated license.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,22 @@
 module Main where
 
+import System.IO
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  hSetBuffering stdout NoBuffering
+  putStr "Enter the cipher key: "
+  key <- getLine
+  putStr "Enter the word to cipher: "
+  word <- getLine
+  putStrLn $ vigenereCipher key word
+
+
+vigenereCipher :: String -> String -> String
+vigenereCipher code = zipWith addChar infiniteCode
+  where
+    infiniteCode = code ++ infiniteCode
+    --Zip the infiniteCode and the word together
+    toInt' c = fromEnum c - fromEnum 'a'
+    toChar' i = toEnum (i + fromEnum 'a')
+    addChar a b = toChar' $ (toInt' a + toInt' b) `mod` 26
